f3ce1ff3172e89fd0e258394638f810530bdb8d6
kovacsfrank98 <kovacsfrank98@gmail.com>
2022-08-14 18:51:25 -0400

    C7: Commit App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Tasks from './todos/todos';
+import TaskForm from './todos/addtodo';
 
 function App() {
   return (
     <div className="App"> 
-
-      
+        <TaskForm />
+        <Tasks />
     </div>
   );
 }
