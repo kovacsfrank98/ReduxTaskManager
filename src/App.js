85bd2d1235e48dac2ec066f96edddfc8305825f5
kovacsfrank98 <kovacsfrank98@gmail.com>
2022-08-14 20:01:41 -0400

    C11: Commit Header, inc, and add Header to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './header';
 import Tasks from './todos/todos';
 import TaskForm from './todos/addtodo';
 
 function App() {
   return (
+    <Header />
     <div className="App"> 
         <TaskForm />
         <Tasks />
