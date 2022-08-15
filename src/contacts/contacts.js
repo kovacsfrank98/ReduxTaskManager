1e1f72e7ef0bad35a512139aa70e94b0407cab93
kovacsfrank98 <kovacsfrank98@gmail.com>
2022-08-15 05:53:08 -0400

    C14: Completed Contacts

--- src/contacts/contacts.js
+++ src/contacts/contacts.js
@@ -7,7 +7,21 @@ function Contact(){
             <h1>Contact Info</h1>
         <form>
             <div>
-            </div>
+                <h3>First Name</h3>
+                <input type="text" id="first" name="first" required>
+            </div><br>
+            <div>
+                <h3>Last Name</h3>
+                <input type="text" id="last" name="last" required>
+            </div><br>
+            <div>
+                <h3>Email</h3>
+                <input type="text" id="email" name="email" required>
+            </div><br>
+            <div>
+                <h3>Comments</h3>
+                <textarea id="comments" name="comments" rows="5" cols="40">Comments...</textarea>
+            </div><br>
         </form>
         </div>
     );
