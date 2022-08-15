66ca8da3d59ed38650b20e54f80a0501acf08776
kovacsfrank98 <kovacsfrank98@gmail.com>
2022-08-14 20:24:47 -0400

    C12: Commit App with Updates

--- src/header.js
+++ src/header.js
@@ -1,5 +1,13 @@
+import App from './App';
+import ??? from './contacts/contacts';
+import {NavLink} from 'react-router-dom';
+
 function Header()
 {
     return (
+        <nav>
+            <NavLink to="/tasks">App</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+        </nav>
     );
 }
